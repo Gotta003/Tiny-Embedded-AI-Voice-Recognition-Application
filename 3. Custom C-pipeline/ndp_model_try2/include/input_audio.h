12327ecbb9226ffbb1bcc0f8b84b736c0618f516
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2025-03-19 22:14:08 +0100

    Pratical Test

--- 3. Custom C-pipeline/ndp_model_try2/include/input_audio.h
+++ 3. Custom C-pipeline/ndp_model_try2/include/input_audio.h
@@ -12,7 +12,7 @@
 #define AUDIO_WINDOW 0.032
 #define DURATION_SECONDS 1
 #define NUM_CHANNELS 1
-#define NUM_SAMPLES (int)(SAMPLE_RATE * DURATION_SECONDS)
+#define NUM_SAMPLES (int)(SAMPLE_RATE * (DURATION_SECONDS))
 #define FFT_SIZE 512
 
 static int audio_callback(const void* inputBuffer, void* outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userData);
@@ -24,5 +24,6 @@ void start_stream(PaStream* stream);
 void stop_stream(PaStream* stream);
 void close_stream(PaStream* stream);
 void terminate_portaudio();
-
+void live_sampling();
+void test_sampling();
 #endif
